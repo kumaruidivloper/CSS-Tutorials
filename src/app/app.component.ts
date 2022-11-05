82cd2e8f49c706d124044936d4e745f3a23f2bbe
Kumar Shan <kumarshan@Kumars-MacBook-Pro.local>
2022-11-05 09:01:40 +0530

    Anolog clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AnalogandDigitalClock';
+
+  @ViewChild('sc') sc!: ElementRef;
+  @ViewChild('mn') mn!: ElementRef;
+  @ViewChild('hr') hr!: ElementRef;
+
+
+  ngOnInit() {
+      this.callEverySec();
+  }
+
+
+  callEverySec() {
+    setInterval(() => {
+      let day = new Date();
+      let hh = day.getHours() * 30;
+      let mm = day.getMinutes() * 6;
+      let ss = day.getSeconds() * 6;
+
+      console.log(ss)
+
+      this.hr.nativeElement.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+      this.mn.nativeElement.style.transform = `rotateZ(${mm}deg)`;
+      this.sc.nativeElement.style.transform = `rotateZ(${ss}deg)`;
+    })
+  }
 }
