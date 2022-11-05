71a5929ac68b0305d332dc82b0366bf55f6ba1ff
Kumar Shan <kumarshan@Kumars-MacBook-Pro.local>
2022-11-05 10:24:58 +0530

    digital clock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,12 @@ export class AppComponent implements OnInit {
   @ViewChild('mn') mn!: ElementRef;
   @ViewChild('hr') hr!: ElementRef;
 
+  @ViewChild('hour') hour!: ElementRef;
+  @ViewChild('minutes') minutes!: ElementRef;
+  @ViewChild('seconds') seconds!: ElementRef;
+  @ViewChild('ampm') ampm!: ElementRef;
+
+
 
   ngOnInit() {
       this.callEverySec();
@@ -20,16 +26,43 @@ export class AppComponent implements OnInit {
 
   callEverySec() {
     setInterval(() => {
-      let day = new Date();
-      let hh = day.getHours() * 30;
-      let mm = day.getMinutes() * 6;
-      let ss = day.getSeconds() * 6;
+      let date = new Date();
+      this.analog(date);
+      this.digital(date);
+    });
+  }
 
-      console.log(ss)
+  analog(date:any) {
+    
+      let hh = date.getHours() * 30;
+      let mm = date.getMinutes() * 6;
+      let ss = date.getSeconds() * 6;
+
 
       this.hr.nativeElement.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
       this.mn.nativeElement.style.transform = `rotateZ(${mm}deg)`;
       this.sc.nativeElement.style.transform = `rotateZ(${ss}deg)`;
-    })
+  }
+
+  digital(date:any) {
+    let hour:number | string = date.getHours();
+    let minutes:number | string = date.getMinutes();
+    let seconds:number | string = date.getSeconds();
+      
+    
+      // add zero befor sigle digit number
+      hour  = (hour < 10) ? '0' + hour : hour
+      minutes  = (minutes < 10) ? '0' + minutes : minutes
+      seconds  = (seconds < 10) ? '0' + seconds : seconds
+
+      // conver 24hr clock to 12hr clock
+      if(hour > 12) {
+        hour = +hour - 12;
+      }
+
+      this.hour.nativeElement.innerHTML = hour;
+      this.minutes.nativeElement.innerHTML = minutes;
+      this.seconds.nativeElement.innerHTML = seconds;
+      this.ampm.nativeElement.innerHTML = hour >= 12 ? 'PM' : 'AM';
   }
 }
